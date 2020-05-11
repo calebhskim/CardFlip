4d67fc9ac2d2547f4edbdb494fafceb89620ab89
calebhskim <caleb.hs.kim@gmail.com>
2020-05-10 19:23:03 -0700

    Updates

---
+++ precache-manifest.563b64bbfd2d083f328e75c9b842a268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfeacebbe3ea1d7842135be59794600c",
+    "url": "/CardFlip/index.html"
+  },
+  {
+    "revision": "26569f6c4f34ac67d32a",
+    "url": "/CardFlip/static/css/main.02508275.chunk.css"
+  },
+  {
+    "revision": "6aa6e77bfd010ac8d818",
+    "url": "/CardFlip/static/js/2.57f1e0b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CardFlip/static/js/2.57f1e0b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26569f6c4f34ac67d32a",
+    "url": "/CardFlip/static/js/main.ba41f470.chunk.js"
+  },
+  {
+    "revision": "c8b3368f74be98ce9762",
+    "url": "/CardFlip/static/js/runtime-main.7b7b22c4.js"
+  }
+]);
